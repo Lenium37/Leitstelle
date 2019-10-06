492d31fb321190bdd24941d255f2545326587399
Johannes Hirth <hirth.johannes@gmx.de>
2019-10-06 12:58:34 +0200

    added class MainWindow

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,26 @@
 // Created by Johannes on 04.10.2019.
 //
 
-#include "main.h"
+#include "main_window.h"
+#include "vehicle_window.h"
+#include <QApplication>
+#include <QtCore/QFile>
+
+int main(int argc, char *argv[]) {
+
+  QApplication a(argc, argv);
+
+  // Setting Stylesheet
+  QFile File(":styles.qss");
+  File.open(QFile::ReadOnly);
+  QString StyleSheet = QLatin1String(File.readAll());
+  a.setStyleSheet(StyleSheet);
+
+  MainWindow w;
+  w.setBaseSize(1920, 1080);
+  w.show();
+  VehicleWindow w2;
+  w2.show();
+
+  return QApplication::exec();
+}
