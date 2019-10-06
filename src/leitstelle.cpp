8e44299e2e3761f3507f5c9ecf27a811b3e3d478
Johannes Hirth <hirth.johannes@gmx.de>
2019-10-06 21:41:00 +0200

    added new main class Leitstelle

--- src/leitstelle.cpp
+++ src/leitstelle.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "leitstelle.h"
+#include "xml_vehicle_list_reader.h"
+#include <QtXml>
+#include <iostream>
+
+Leitstelle::Leitstelle() {
+  this->m_main_window.setWindowState(Qt::WindowMaximized);
+  this->m_vehicle_window.setWindowState(Qt::WindowMaximized);
+  this->m_vehicle_window.show();
+
+  this->init();
+}
+
+int Leitstelle::init() {
+  this->read_vehicles_from_xml();
+  return 0;
+}
+
+int Leitstelle::read_vehicles_from_xml() {
+  // Open a file for reading
+  QFile file(":vehicles.xml");
+  if(!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    return -1;
+  }
+
+  XmlVehicleListReader::read(&file, this->m_vehicles_fire, this->m_vehicles_ems);
+  std::cout << "number of fire vehicles: " << this->m_vehicles_fire.size() << std::endl;
+  std::cout << "number of ems vehicles: " << this->m_vehicles_ems.size() << std::endl;
+}
+
+Leitstelle::~Leitstelle() = default;
+
+std::map<const std::string, std::shared_ptr<Vehicle>> Leitstelle::get_vehicles_fire() {
+  return this->m_vehicles_fire;
+}
+
+std::map<const std::string, std::shared_ptr<Vehicle>> Leitstelle::get_vehicles_ems() {
+  return this->m_vehicles_ems;
+}
+
+void Leitstelle::add_vehicle(std::shared_ptr<Vehicle> vehicle, bool is_fire) {
+  if(is_fire)
+    this->m_vehicles_fire.insert(std::pair<std::string, std::shared_ptr<Vehicle>>(vehicle->get_name(), vehicle));
+}
+
--- src/leitstelle.h
+++ src/leitstelle.h
@@ -5,12 +5,30 @@
 #ifndef LEITSTELLE_SRC_LEITSTELLE_H_
 #define LEITSTELLE_SRC_LEITSTELLE_H_
 
-
+#include "vehicle_window.h"
+#include "main_window.h"
+#include "vehicle.h"
 
 class Leitstelle {
+ private:
+  MainWindow m_main_window;
+  VehicleWindow m_vehicle_window;
+  //MessageWindow m_message_window;
+  //InfoWindow m_info_window;
 
+  int init();
+  int read_vehicles_from_xml();
+
+  std::map<const std::string, std::shared_ptr<Vehicle>> m_vehicles_fire;
+  std::map<const std::string, std::shared_ptr<Vehicle>> m_vehicles_ems;
+
+ public:
+  Leitstelle();
+  ~Leitstelle();
+
+  std::map<const std::string, std::shared_ptr<Vehicle>> get_vehicles_fire();
+  std::map<const std::string, std::shared_ptr<Vehicle>> get_vehicles_ems();
+  void add_vehicle(std::shared_ptr<Vehicle> vehicle, bool is_fire);
 };
-
-
 
 #endif //LEITSTELLE_SRC_LEITSTELLE_H_
